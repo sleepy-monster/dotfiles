9232d64b25e9ed33727d001b5ce7c5200bc8cd85
akagi146 <15861387+akagi146@users.noreply.github.com>
2021-11-15 12:09:10 +0900

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+####################################
+# Usage:
+# ./mac.sh
+####################################
+
+# Install Homebrew
+
+
+
+# Install Packages
+brew bundle --file=config_mac/.Brewfile
+
+
+
+
