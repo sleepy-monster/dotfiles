f946908068e987222e7d5dcb2f723853368a37b7
akagi146 <15861387+akagi146@users.noreply.github.com>
2021-11-25 14:44:15 +0900

    Add files via upload
    インストールスクリプトの作成

---
+++ dotfiles/bin/install.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+from os.path import expanduser
+import datetime
+import os
+import subprocess
+import shutil
+import sys
+import glob
+
+
+# 指定したディレクトリ内のファイル一覧を取得する
+def list_dotfiles(directory):
+    files = glob.glob(directory + "/.*")
+    return files
+
+
+def main():
+    # ホームディレクトリを取得
+    home = expanduser("~")
+    old_dotfiles = list_dotfiles(home)
+
+    # backupディレクトリの作成
+    now_date = (datetime.datetime.now()).strftime('%Y%m%d%H%M')
+    dotfiles_backup_dir = home + '/dotfiles_backup/' + now_date
+    os.makedirs(dotfiles_backup_dir, exist_ok=True)
+
+#    # dotfileをバックアップ
+#    for f in old_dotfiles:
+#        shutil.move(f, dotfiles_backup_dir)
+
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    parent_script_dir = os.path.join(os.path.dirname(script_dir), './')
+    parent_script_dir = os.path.normpath(parent_script_dir)
+    new_dotfiles = list_dotfiles(parent_script_dir)
+
+    with open(os.path.join(parent_script_dir, '.gitignore_global'), "r") as f:
+        list = f.read().split('\n')
+        # シンボリックリンク作成
+        for n in new_dotfiles:
+            basename = os.path.basename(n)
+            if basename not in list:
+                link_path = os.path.join(home, basename)
+
+                if os.path.exists(link_path):
+                    shutil.move(link_path, dotfiles_backup_dir)
+
+                os.symlink(n, link_path)
+
+    # コマンド実行
+    command = []
+    command.append(['git', 'config', '--global', 'core.excludesfile',
+                    os.path.join(home, '.gitignore_global')])
+
+    for c in command:
+        subprocess.call(c)
+
+
+if __name__ == '__main__':
+    main()
