d40569050b27f58a222951ff4b96e87159c25463
akagi146 <15861387+akagi146@users.noreply.github.com>
2022-10-16 05:19:58 +0900

    最新の状態に更新＆インストールパッケージの追加

---
+++ config_mac/.zprofile
@@ -0,0 +1,6 @@
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# aliases
+if [ -f ~/.zsh_aliases ]; then
+ . ~/.zsh_aliases
+fi
--- mac.sh
+++ mac.sh
@@ -6,12 +6,14 @@
 ####################################
 
 # Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
+
+
+# source "${HOME}/.zsh_aliases"
 
 
 # Install Packages
-brew bundle --file=config_mac/.Brewfile
-
-
-
-
+brew bundle --file=config_mac/Brewfile
